ecc9d0c9cb537c78a850910743b9868634f66bc4
Himanshu <sangwanhimanshu8443@gmail.com>
2026-01-30 20:57:40 +0530

    chore: bump version to 3.3.2 and update dependencies

--- app.config.js
+++ app.config.js
@@ -95,7 +95,7 @@ module.exports = () => {
       autolinking: {exclude: ['expo-splash-screen']},
       plugins,
       slug: 'vega',
-      version: '3.3.1',
+      version: '3.3.2',
       userInterfaceStyle: 'dark',
       experiments: {
         reactCompiler: true,
@@ -107,7 +107,7 @@ module.exports = () => {
         minSdkVersion: 24,
         edgeToEdgeEnabled: true,
         package: 'com.vega',
-        versionCode: 158,
+        versionCode: 160,
         permissions: [
           'FOREGROUND_SERVICE',
           'FOREGROUND_SERVICE_MEDIA_PLAYBACK',
